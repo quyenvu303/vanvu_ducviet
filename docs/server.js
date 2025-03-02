50b95242bf4073662dcca43a764e3cf372567916
Đình Quyền <75819095+quyenvu303@users.noreply.github.com>
2025-03-02 11:49:28 +0700

    'add_1'

--- docs/server.js
+++ docs/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("docs")); // Chạy thư mục "docs" chứa index.html
+// Serve các file tĩnh trong thư mục DOCS
+app.use(express.static(path.join(__dirname, "DOCS")));
 
 app.listen(PORT, () => {
-    console.log(`Server đang chạy tại http://localhost:${PORT}`);
+  console.log(`Server đang chạy tại http://localhost:${PORT}`);
 });
