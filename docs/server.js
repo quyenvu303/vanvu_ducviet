75fa3c604de54fd0f71aea62a486830e098fe844
Đình Quyền <75819095+quyenvu303@users.noreply.github.com>
2025-03-02 11:59:23 +0700

    'add_4'

--- docs/server.js
+++ docs/server.js
@@ -4,8 +4,13 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve các file tĩnh trong thư mục DOCS
-app.use(express.static(path.join(__dirname, "docs")));
+// Sử dụng thư mục hiện tại (docs) làm thư mục tĩnh
+app.use(express.static(__dirname));
+
+// Route mặc định để tránh lỗi "Cannot GET /"
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server đang chạy tại http://localhost:${PORT}`);
